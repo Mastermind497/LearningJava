a47b7b08aac79f0c13a19fba0a29b74e7e2fab58
Shourya Bansal <shouryabansal2012@gmail.com>
2020-07-29 16:45:21 -0400

    Finished Rock Paper Scissors

--- src/Classes/PlayRPS.java
+++ src/Classes/PlayRPS.java
@@ -4,5 +4,7 @@ public class PlayRPS {
     public static void main(String[] args) {
         RPS one = new RPS("One");
         RPS two = new RPS("Two");
+
+        System.out.println(RPS.fight(one, two));
     }
 }
--- src/Classes/RPS.java
+++ src/Classes/RPS.java
@@ -2,9 +2,9 @@ package Classes;
 
 public class RPS {
 
-    private static final String ROCK     = "R"; //0
-    private static final String SCISSORS = "S"; //1
-    private static final String PAPER    = "P"; //2
+    private static final String ROCK     = "Rock"; //0
+    private static final String SCISSORS = "Scissors"; //1
+    private static final String PAPER    = "Paper"; //2
 
     private String strategy;
     private final String name;
@@ -15,6 +15,10 @@ public class RPS {
 
     public String getName() {
         return this.name;
+    }
+
+    public String getStrategy() {
+        return this.strategy;
     }
 
     public void randomizeStrategy() {
@@ -36,5 +40,40 @@ public class RPS {
         return (int)(Math.random() * 3);
     }
 
+    public boolean beats(RPS b) {
+        if (this.getStrategy().equals(ROCK)) {
+            if (b.getStrategy().equals(PAPER)) {
+                return false;
+            } else {
+                return true;
+            }
+        } else if (this.getStrategy().equals(PAPER)) {
+            if (b.getStrategy().equals(ROCK)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (b.getStrategy().equals(PAPER)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static String fight(RPS a, RPS b) {
+        a.randomizeStrategy();
+        b.randomizeStrategy();
+
+        if (a.getStrategy().equals(b.getStrategy())) {
+            return fight(a, b);
+        } else if (a.beats(b)) {
+            return (a.getName() + " beat " + b.getName() + " because " + a.getStrategy() + " beats " + b.getStrategy());
+        }
+        else {
+            return (b.getName() + " beat " + a.getName() + " because " + b.getStrategy() + " beats " + a.getStrategy());
+        }
+    }
 
 }
